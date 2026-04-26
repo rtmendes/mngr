503a44aa4e44d7f535ff02dca794fd12314184f7
Evan Ryan Gunter <evgunter@gmail.com>
2026-04-25 19:41:55 -0700

    Revert "Reset _plugin_config_registry in autouse plugin_manager fixture"
    This reverts commit 9415fa79fd04e1de410e76e0c36c8bb41295ea47.

--- libs/mngr/imbue/mngr/conftest.py
+++ libs/mngr/imbue/mngr/conftest.py
@@ -25,7 +25,6 @@ from imbue.mngr.api.providers import reset_provider_instances
 from imbue.mngr.config.consts import PROFILES_DIRNAME
 from imbue.mngr.config.data_types import MngrConfig
 from imbue.mngr.config.data_types import MngrContext
-from imbue.mngr.config.plugin_registry import reset_plugin_config_registry
 from imbue.mngr.hosts.host import Host
 from imbue.mngr.plugin_catalog import get_independent_entry_point_names
 from imbue.mngr.plugins import hookspecs
@@ -497,7 +496,6 @@ def plugin_manager(
     reset_backend_registry()
     reset_agent_registry()
     reset_provider_instances()
-    reset_plugin_config_registry()
 
     # Discover all entry-point plugins and block everything except enabled_plugins
     all_eps = {ep.name for ep in importlib.metadata.entry_points(group="mngr")}
@@ -524,7 +522,6 @@ def plugin_manager(
     reset_backend_registry()
     reset_agent_registry()
     reset_provider_instances()
-    reset_plugin_config_registry()
 
 
 # =============================================================================
